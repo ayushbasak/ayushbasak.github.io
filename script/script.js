e707aa22260327bbccd723274f6429812f526361
Ayush Basak <ayushbasak0210@gmail.com>
2020-03-17 12:34:23 +0530

    Changes

--- script/script.js
+++ script/script.js
@@ -7,13 +7,13 @@ function scrollFunction() {
 	var x = document.getElementById("top");
 	var goTOPbutton = document.getElementById("goTOP");
   if (document.body.scrollTop > 30 || document.documentElement.scrollTop > 30) {
-    x.style.fontSize = "20px";
-	x.style.padding = "10px";
+    x.style.fontSize = "10px";
+	x.style.padding = "20px";
 	goTOPbutton.style.display = "block";
   } else {
     x.style.fontSize = "30px";
 	goTOPbutton.style.display = "none";
-	x.style.padding = "10px";
+	x.style.objectFit = "cover";
   }
 
 }
