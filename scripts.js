3cab8fa7cc584c99edceabfd5c2a790da4107ad5
ayushbasak <ayushbasak0210@gmail.com>
2021-08-25 12:03:46 +0530

    [Add] Dark Theme Controller

--- scripts.js
+++ scripts.js
@@ -29,3 +29,26 @@ nav_interests.addEventListener('click', function() {
 
     document.getElementById('interests').style.display = "block";
 })
+
+/**
+ * Theme Controller
+*/
+
+
+const container = document.getElementsByClassName('container');
+const theme = document.getElementsByClassName('theme');
+
+let darkTheme = false;
+
+theme[0].addEventListener('click', function() {
+    if(darkTheme) {
+        container[0].style.filter = 'grayscale(1)';
+        document.body.style.background = "#fff";
+    }
+    else{
+        container[0].style.filter = 'grayscale(1) invert(1)';
+        document.body.style.background = "#000";
+    }
+    //toggle darktheme
+    darkTheme = !darkTheme;
+})
